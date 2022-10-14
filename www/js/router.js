ca18a2c3880f1cc6e03cfb551da582d2592bcccf
nandoti <nando.frontend@gmail.com>
2022-10-14 14:41:49 -0300

    refactor: alterações nas configs

--- www/js/router.js
+++ www/js/router.js
@@ -1,40 +1,39 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 var app = new Framework7({
-    // App root element
-    el: '#app',
-    // App Name
-    name: 'Betha Assistente',
-    // App id
-    id: 'br.com.betha.assistente',
-    // Enable swipe panel
-    panel: {
-      swipe: true,
+  // App root element
+  el: '#app',
+  // App Name
+  name: 'Ju Assistente',
+  // App id
+  id: 'br.com.Ju.assistente',
+  // Enable swipe panel
+  panel: {
+    swipe: true,
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/index/',
+      url: 'index.html',
+      on: {
+        pageInit: function (e, page) {
+          $.getScript('js/index.js');
+        },
+      }
     },
-    // Add default routes
-    routes: [
-      {
-        path: '/index/',
-        url: 'index.html',
-        on: {
-            pageInit: function (e, page) {
-                 $.getScript('js/index.js');      
-            },
-        }
-      },
-      {
-        path: '/memorias/',
-        url: 'memorias.html',
-        on: {
-            pageInit: function (e, page) {
-              $.getScript('js/memorias.js');                        
-            },
-        }        
-      },
-    ],
-    // ... other parameters
-  });
-  
-  function onDeviceReady(){
-    var mainView = app.views.create('.view-main',{url:'/index/'});
-  }
-  
+    {
+      path: '/memorias/',
+      url: 'memorias.html',
+      on: {
+        pageInit: function (e, page) {
+          $.getScript('js/memorias.js');
+        },
+      }
+    },
+  ],
+  // ... other parameters
+});
+
+function onDeviceReady() {
+  var mainView = app.views.create('.view-main', { url: '/index/' });
+}
