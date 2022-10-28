c0951505b70c61a7377bf144d1bca70d748482a4
nandoti <nando.frontend@gmail.com>
2022-10-28 15:16:41 -0300

    refactor: refatoração

--- www/js/index.js
+++ www/js/index.js
@@ -1,74 +1,104 @@
-//ASSIM QUE A ROTA INDEX COMEÇAR
-//VERIFICAR SE TEM PERMISSAO DE USAR SPEECHRECOGNITION
+$.getScript("js/comandos.js");
+
+if (
+    localStorage.getItem("banco") !== null &&
+    localStorage.getItem("banco") !== ""
+) {
+    banco = JSON.parse(localStorage.getItem("banco"));
+    temBanco = true;
+} else {
+    console.log("Não tem um banco de dados ainda!");
+    temBanco = false;
+}
+
 window.plugins.speechRecognition.hasPermission(
     function (permissao) {
-        //SE NÃO TIVER PERMISSAO
         if (!permissao) {
-            //SOLICITAR A PERMISSÃO
             window.plugins.speechRecognition.requestPermission(
                 function (temPermissao) {
-                    app.dialog.alert('Permissão concedida: ' + temPermissao);
-                }, function (erro) {
-                    app.dialog.alert('Request Permission error: ' + erro);
-                })
-
+                    app.dialog.alert("Permissão concedida: " + temPermissao);
+                },
+                function (erro) {
+                    app.dialog.alert("Request Permission error: " + erro);
+                }
+            );
         }
-    }, function (error) {
-        app.dialog.alert('hasPermission error: ' + error);
-    })
+    },
+    function (error) {
+        app.dialog.alert("hasPermission error: " + error);
+    }
+);
 
-//CLICOU NO BOTÃO FALAR
-$("#BtnFalar").on('click', function () {
+$("#BtnFalar").on("click", function () {
     let options = {
         language: "pt-BR",
         showPopup: false,
-        showPartial: true
-    }
+        showPartial: true,
+    };
 
-    //COMEÇOU A "ESCUTAR"
     window.plugins.speechRecognition.startListening(
-        //SE SUCESSO
         function (dados) {
             $.each(dados, function (index, texto) {
-                //COLOCAR O QUE ELA ENTENDE NO P CHAMADO PERGUNTA
                 $("#pergunta").html("").append(texto);
-                //PEGAR O VALOR DO QUE ELA ENTENDEU
+
                 var pergunta = $("#pergunta").html().toLowerCase();
+                console.log("Ela entendeu: " + pergunta);
 
-                //VERIFICAR SE O COMANDO É ESSE
-                if (pergunta == "acessar memórias" || pergunta == "acessar memória") {
-                    app.views.main.router.navigate('/memorias/');
-                }
-
-                if (pergunta == "qual é o seu nome" || pergunta == "qual é seu nome") {
-                    // or with more options
-                    TTS.speak({
-                        text: 'Meu nome é Ju',
-                        locale: 'pt-BR',
-                        rate: 0.75
-                    }, function () {
-                        //SE ELA FALAR ESCREVER NA TELA A RESPOSTA
-                        var typed = new Typed('#resposta', {
-                            strings: ['Meu nome é Ju ^1000', ''],
-                            typeSpeed: 40,
-                            showCursor: false,
-                            onComplete: function (self) {
-                                toastBottom = app.toast.create({
-                                    text: 'Fala concluída com sucesso!',
-                                    closeTimeout: 2000,
-                                });
-                                toastBottom.open();
-                            }
-
-                        });
-                    }, function (erro) {
-                        app.dialog.alert('Houve um erro: ' + erro);
+                if (temBanco) {
+                    $.each(banco, function (index, item) {
+                        if (pergunta == item.p_falada) {
+                            falar(item.r_escrita, item.r_falada, false);
+                        }
                     });
                 }
-            })
+
+                $.each(comandos, function (index, item) {
+                    if (pergunta == item.entrada) {
+                        if (item.especial) {
+                            item.especial();
+                        } else {
+                            falar(item.retorno_escrito, item.retorno_falado, item.rota);
+                        }
+                    }
+                });
+            });
         },
-        //SE DER ERRO
+
         function (erro) {
-            app.dialog.alert('Houve um erro: ' + erro);
-        }, options)
+            app.dialog.alert("Houve um erro: " + erro);
+        },
+        options
+    );
 });
+
+function falar(r_escrita, r_falada, rota) {
+    TTS.speak(
+        {
+            text: r_falada,
+            locale: "pt-BR",
+            rate: 0.75,
+        },
+        function () {
+            console.log("A assistente falou: " + r_escrita);
+            if (rota) {
+                app.views.main.router.navigate(rota);
+            }
+        },
+        function (erro) {
+            app.dialog.alert("Houve um erro: " + erro);
+        }
+    );
+
+    var typed = new Typed("#resposta", {
+        strings: [r_escrita + "^1000", ""],
+        typeSpeed: 40,
+        showCursor: false,
+        onComplete: function (self) {
+            toastBottom = app.toast.create({
+                text: "Fala concluída com sucesso!",
+                closeTimeout: 2000,
+            });
+            toastBottom.open();
+        },
+    });
+}
